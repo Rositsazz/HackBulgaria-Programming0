2e43c86c93665767fc8011a6a53c007ccd81f818
Rositsazz <ross_zz@mail.bg>
2015-03-14 22:33:51 +0200

    Update simple_answers.py

--- week1/And Or Not In Problems/simple_answers.py
+++ week1/And Or Not In Problems/simple_answers.py
@@ -5,15 +5,17 @@ search3 = "how are you?"
 search4 = "feelings"
 search5 = "age"
 
-if search1 in string :
+if search1 or search2 in string :
     print("Hello there, good stranger!")
-elif search2 in string :
-    print("Hello there, good stranger!")
+
 elif search3 in string :
     print("I am fine, thanks. How are you?")
+
 elif search4 in string :
     print("I am a machine. I have no feelings")
+
 elif search5 in string :
     print("I have no age. Only current timestamp")
+
 else :
     print("Not such word")
