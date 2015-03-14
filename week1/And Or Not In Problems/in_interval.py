8a533de8886a35c1169571dcc38253ff749afcff
Rositsazz <ross_zz@mail.bg>
2015-03-14 22:30:59 +0200

    Update in_interval.py

--- week1/And Or Not In Problems/in_interval.py
+++ week1/And Or Not In Problems/in_interval.py
@@ -1,6 +1,6 @@
 a=input('Enter the lower bound ''a'': ')
 b=input('Enter the upper bound ''b'': ')
-x=input('Enter the number ''x'': ')
+x=input('Enter number ''x'': ')
 a=int(a)
 b=int(b)
 x=int(x)
@@ -8,14 +8,18 @@ x=int(x)
 if a < b:
     if x == a :
         print('The number is equal to the lower bound of the interval')
+
     elif x == b :
         print('The number is equal to the upper bound of the interval')
+
     elif a < x and x < b :
         print('The number is in the interval')
+
     elif x < a :
         print('The number is outside the interval, x < a')
+
     else :
         print('The number is outside the interval, x > b')
+
 elif a>b or a==b :
     print('Invalid lower or upper bound')
-    
