70c3d470f50f304a6fb31f43e9ccb514b79a5760
Rositsazz <ross_zz@mail.bg>
2015-03-14 22:49:23 +0200

    Update count_to_ten.py

--- week1/While Loop Problems/count_to_ten.py
+++ week1/While Loop Problems/count_to_ten.py
@@ -2,9 +2,10 @@ counter = 1
 
 while counter <= 10 :
     print(counter)
-    counter = counter + 1
+    counter += 1
 
 counter = 10
+
 while counter >= 1 :
     print(counter)
-    counter = counter - 1
+    counter -= 1
