b4e7074f9dee614961e68ee73828c6da8d11a432
Rositsazz <ross_zz@mail.bg>
2015-03-14 22:50:18 +0200

    Update count_to_user.py

--- week1/While Loop Problems/count_to_user.py
+++ week1/While Loop Problems/count_to_user.py
@@ -1,12 +1,14 @@
 number = input("Enter a number: ")
 number = int(number)
+
 counter = 1
 
 while counter <= number :
     print(counter)
-    counter = counter + 1
+    counter += 1
 
 counter = number
+
 while counter >= 1 :
     print(counter)
-    counter = counter - 1
+    counter -= 1
