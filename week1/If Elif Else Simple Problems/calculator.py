0a79722751565aa53e50efa32d161166bed5fcb1
Rositsazz <ross_zz@mail.bg>
2015-03-14 22:42:47 +0200

    Update calculator.py

--- week1/If Elif Else Simple Problems/calculator.py
+++ week1/If Elif Else Simple Problems/calculator.py
@@ -1,13 +1,21 @@
 number1 = input ('Enter first number: ')
 number2 = input ('Enter second number: ')
+
+number1 = int(number1)
+number2 = int(number2)
+
 oper = input('Enter an operator(choose from +, -, * or /)')
 if oper == '+':
-    print(int(number1)+int(number2))
+    print(number1+number2)
+
 elif oper == '-':
-    print(int(number1)-int(number2))
+    print(number1-number2)
+
 elif oper == '*':
-    print(int(number1)*int(number2))
+    print(number1)*number2)
+
 elif oper == '/':
-    print(int(number1)/int(number2))
+    print((number1)/number2)
+    
 else:
     print('We do not support that operation.')
