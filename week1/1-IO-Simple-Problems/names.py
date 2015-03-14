ce2773868484b8460bd5cca68982205f702e3602
Rositsazz <ross_zz@mail.bg>
2015-03-14 22:13:02 +0200

    Update names.py

--- week1/1-IO-Simple-Problems/names.py
+++ week1/1-IO-Simple-Problems/names.py
@@ -1,11 +1,9 @@
-first_name = input('Your first name is? :')
-#print ('Fisrt name %s \n' % (first_name))
-second_name = input('Your second name is? :')
-#print ('Second name %s \n' % (second_name))
-third_name = input('Your third name is? :')
-#print ('Third name %s \n' % (third_name))
+first_name = input('Your first name is?: ')
+#same //  print ('Fisrt name %s \n' % (first_name))
+second_name = input('Your second name is?: ')
+#same //  print ('Second name %s \n' % (second_name))
+third_name = input('Your third name is?: ')
+#same //  print ('Third name %s \n' % (third_name))
 print ('Your name is {0} {1} {2}\n'.format( first_name,second_name, third_name))
 
-#the same
-
-#print ('Your name is ',first_name,second_name, third_name)
+#same //  print ('Your name is ',first_name,second_name, third_name)
