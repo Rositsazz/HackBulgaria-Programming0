4d8ab8a92cc073cada1043659d1bca0fdfa611f1
Rositsazz <ross_zz@mail.bg>
2015-03-14 23:08:26 +0200

    Update bigger_last_digit.py

--- week1/Saturday Tasks/bigger_last_digit.py
+++ week1/Saturday Tasks/bigger_last_digit.py
@@ -3,11 +3,17 @@ a = int(a)
 b = input("Enter the second number:")
 b = int(b)
 
-if a%10 > b%10 :
+last_a = a%10
+last_b = b%10
+
+if last_a > last_b :
     print(a)
-if b%10 > a%10 :
+
+if last_b > last_a :
     print(b)
-if a%10 == b%10 :
+
+if last_b == b%10 :
+
     if a>=b :
         print(a)
     else:
