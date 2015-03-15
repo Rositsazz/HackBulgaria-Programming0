9c135bab98419675ec65e52896520cd4092492c0
Rositsazz <ross_zz@mail.bg>
2015-03-15 20:59:16 +0200

    Create while_traverse.py

---
+++ week2/List Problems/while_traverse.py
@@ -0,0 +1,15 @@
+books = ["Learn You a Haskell",
+"The Healthy Programmer",
+"Code Complete",
+"The Pragmatic Programmer",
+"Pro Git",
+"Introduction to Algorithms",
+"Concrete Mathematics"]
+
+i = 0
+length = len(books) - 1
+
+while i <= length:
+    book = books[i]
+    print(book)
+    i += 1
