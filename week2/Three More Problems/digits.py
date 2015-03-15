2f05c454f3fc28ee22f8193d93b399f6c6d563d6
Rositsazz <ross_zz@mail.bg>
2015-03-15 12:45:24 +0200

    Update digits.py

--- week2/Three More Problems/digits.py
+++ week2/Three More Problems/digits.py
@@ -1,16 +1,22 @@
 number = input("Please, enter a number n: ")
 number = int(number)
+
 list_of_digits = []
+
 while number!=0 :
     digit = number%10
     list_of_digits += [digit]
     number = number//10
+    
 list_of_digits.reverse()
+
 print ("List of digits is: " ,list_of_digits)
 
 i = 0
 n = 0 
+
 while i< len(list_of_digits) :
     n = n * 10 + list_of_digits[i]
     i+=1
+    
 print(n)
