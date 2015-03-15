b7d668f13d50c7d84ba2e767388f42db16d86993
Rositsazz <ross_zz@mail.bg>
2015-03-15 21:02:04 +0200

    Update only_evens.py

--- week2/List Problems/only_evens.py
+++ week2/List Problems/only_evens.py
@@ -3,14 +3,20 @@ n = int(n)
 
 even = []
 count = 0
+
 while count < n :
+
     m = input("enter number: " )
-    m=int(m)
+    m = int(m)
+
     if m%2==0 :
-        even = even + [m]
-    count+=1
+        even += [m]
+    count += 1
+
 lenght_of_evens = len(even)
+
 print("Count of evens: " + str(lenght_of_evens))
 print("Evens are:")
+
 for even_number in even :
     print(even_number)
