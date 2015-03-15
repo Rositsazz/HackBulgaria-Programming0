fdb510fffd362c9c0b73cbcb65dc2807be3cc04c
Rositsazz <ross_zz@mail.bg>
2015-03-15 12:46:39 +0200

    Update count_vowels.py

--- week2/Three More Problems/count_vowels.py
+++ week2/Three More Problems/count_vowels.py
@@ -1,8 +1,12 @@
 string = input("Please, enter text: ")
 text = string
+
 vowels = "aeiouyAEIOUY"
 counter = 0 
-for ch in string:
+
+for ch in string :
+    
     if ch in vowels :
         counter+=1
+        
 print("\"" + text + "\"" + " - " + str(counter))
